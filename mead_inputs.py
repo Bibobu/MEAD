cfa0eb35e249477893df6f0bed0484c037d74346
Germain Clavier <germain.clavier@unicaen.fr>
2025-06-20 09:28:16 +0200

    Create mead_inputs.py

---
+++ mead_inputs.py
@@ -0,0 +1,23 @@
+mead_lattice_create = 'fcc'
+mead_lattice_spacing = 4.405
+
+# The number of species to consider
+nattype = 5
+kim_model = 'Sim_LAMMPS_ADP_StarikovGordeevLysogorskiy_2020_SiAuAl__SM_113843830602_000'
+units = 'metal'
+atnames = ['Al', 'Al', 'Al', 'Al', 'Al']
+isghost = [False, False, True, True, True]
+phantom_to_real = [0, 0, 2, 2, 2]
+
+atmasses = [26.981, 26.981 ,26.981, 26.981, 26.981]
+
+radius_surf = 1
+ratio = 0.001
+ecutoff = 0.15
+LoopMax = 1
+Temp = 300.
+
+lat = 0.7
+
+natom_max = 40000
+nloop_max = 10000
