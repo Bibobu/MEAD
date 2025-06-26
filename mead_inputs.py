7d49a8de68662e18a6d09bf6f4addee89e60228d
Germain Clavier <germain.clavier@unicaen.fr>
2025-06-26 09:17:16 +0200

    Update mead_inputs.py, changed the ratio format for new use.

--- mead_inputs.py
+++ mead_inputs.py
@@ -6,13 +6,13 @@ nattype = 5
 kim_model = 'Sim_LAMMPS_ADP_StarikovGordeevLysogorskiy_2020_SiAuAl__SM_113843830602_000'
 units = 'metal'
 atnames = ['Al', 'Al', 'Al', 'Al', 'Al']
-isghost = [False, False, True, True, True]
-phantom_to_real = [0, 0, 2, 2, 2]
+isghost = [False, False, False, True, True]
+phantom_to_real = [0, 0, 0, 2, 3]
 
 atmasses = [26.981, 26.981 ,26.981, 26.981, 26.981]
 
 radius_surf = 1
-ratio = 0.001
+ratio = [0.95, 0.05]
 ecutoff = 0.15
 LoopMax = 1
 Temp = 300.
